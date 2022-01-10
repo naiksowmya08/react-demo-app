bfbadf4e6bbc7fd36bb2fbe6f4c12f124ff33761
sowmya08 <sowmyanaik1996@gmail.com>
2022-01-10 22:14:15 +0530

    demo react app example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import ExpenseItem from './Components/ExpenseItem';
 function App() {
+    const outborder = {
+        border: '5px solid #17bfcc',
+    }
+    const ExpenseArr = [
+        {
+            name: "item1",
+            price: 200,
+            date: "20-10-2021"
+        },
+        {
+            name: "item2",
+            price: 300,
+            date: "20-11-2021"
+        },
+        {
+            name: "item3",
+            price: 500,
+            date: "20-10-2021"
+        }
+    ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <ExpenseItem title={ExpenseArr[0].name} price={ExpenseArr[0].price} dop={ExpenseArr[0].date} />
+          <ExpenseItem title={ExpenseArr[1].name} price={ExpenseArr[1].price} dop={ExpenseArr[1].date} />
+          <ExpenseItem title={ExpenseArr[2].name} price={ExpenseArr[2].price} dop={ExpenseArr[2].date} />
+      </div>
   );
 }
 
---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+function ExpenseItem(props) {
+    const outborder = {
+        border: '5px solid #17bfcc',
+        margin: '0px 0px 20px 0px'
+    };
+    return (
+        <div>
+            <div className="container">
+                <div className="row" style={outborder}>
+                    <div className="col-md-4 divStyle">
+                        <p>{props.title}</p>
+                    </div>
+                    <div className="col-md-4 divStyle">
+                        <p>{props.price}</p>
+                    </div>
+                    <div className="col-md-4 divStyle">
+                        <p>{props.dop}</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+    export default ExpenseItem;
